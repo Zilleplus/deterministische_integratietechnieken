8cc3b4bd3f8e3114044b973fbaa8089347718b25
Willem Melis <zilleplus@gmail.com>
2017-04-26 07:59:45 +0200

    cleaned up code

--- vraag2.m
+++ vraag2.m
@@ -9,28 +9,33 @@ f5 = @(x) exp(-x.^(-2)); %2/exp-2*sqrt(pi)*erfc(1)
 f6 = @(x) abs(x).^3; %1/2
 
 f = {f1 f2 f3 f4 f5 f6};
-eval = [2/21 exp(1)-1/exp(1) sqrt(pi)*erf(1) 1/2*atan(4) 2/exp(1)-2*sqrt(pi)*erfc(1) 1/2];
+analytical_integral_value = [2/21 exp(1)-1/exp(1) sqrt(pi)*erf(1) ...
+    1/2*atan(4) 2/exp(1)-2*sqrt(pi)*erfc(1) 1/2];
 
-errorG = [];
-errorCC = [];
+errorGauss = [];
+errorClenshawCurtis = [];
 iterG = zeros(6,1);
 iterCC = zeros(6,1);
 figure
-for i = 1:6
-    for n = 1:30
-        errorG(i,n) = abs(vraag2_gauss(f{i},n) - eval(i))/abs(eval(i));
-        errorCC(i,n) = abs(vraag2_clenshaw_curtis(f{i},n) - eval(i))/abs(eval(i));
-        if (-log10(errorG(i,n)) >= 7 && iterG(i)==0)
-            iterG(i) = n;
+for index_function = 1:6
+    for numberOfSamples = 1:30
+        errorGauss(index_function,numberOfSamples) = ...
+            abs(vraag2_gauss(f{index_function},numberOfSamples)...
+            - analytical_integral_value(index_function))/abs(analytical_integral_value(index_function));
+        errorClenshawCurtis(index_function,numberOfSamples) = ...
+            abs(vraag2_clenshaw_curtis(f{index_function},numberOfSamples) - ...
+            analytical_integral_value(index_function))/abs(analytical_integral_value(index_function));
+        if (-log10(errorGauss(index_function,numberOfSamples)) >= 7 && iterG(index_function)==0)
+            iterG(index_function) = numberOfSamples;
         end
-        if (-log10(errorCC(i,n)) >= 7 && iterCC(i)==0)
-            iterCC(i) = n;
+        if (-log10(errorClenshawCurtis(index_function,numberOfSamples)) >= 7 && iterCC(index_function)==0)
+            iterCC(index_function) = numberOfSamples;
         end
     end
-    subplot(3,2,i)
-    semilogy(errorG(i,:))
+    subplot(3,2,index_function)
+    semilogy(errorGauss(index_function,:))
     hold on
-    semilogy(errorCC(i,:),'r')
+    semilogy(errorClenshawCurtis(index_function,:),'r')
 end
 
 
