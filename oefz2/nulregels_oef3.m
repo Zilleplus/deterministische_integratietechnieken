de749ff3bc4bc20f0edbbcd83d396924538344c8
Willem Melis <zilleplus@gmail.com>
2017-05-13 22:50:25 +0200

    only odd rules should be used offcourse

--- oefz2/nulregels_oef3.m
+++ oefz2/nulregels_oef3.m
@@ -2,23 +2,24 @@ close all; clear;
 run('functies');
 number_of_points=31;
 
-N1 = zeros(30,1);
+N1 = zeros(14,1);
 N2 = N1;N3 = N1;N4 = N1;
-for degree_nullRulle = 1:30
-    N1(degree_nullRulle) = nullrule(-1, 1, number_of_points, f1,degree_nullRulle);
-    N2(degree_nullRulle) = nullrule(-1, 1, number_of_points, f2,degree_nullRulle);
-    N3(degree_nullRulle) = nullrule(-1, 1, number_of_points, f3,degree_nullRulle);
-    N4(degree_nullRulle) = nullrule(-1, 1, number_of_points, f4,degree_nullRulle);
+for i = 1:15
+    N1(i) = nullrule(-1, 1, number_of_points, f1,i*2-1);
+    N2(i) = nullrule(-1, 1, number_of_points, f2,i*2-1);
+    N3(i) = nullrule(-1, 1, number_of_points, f3,i*2-1);
+    N4(i) = nullrule(-1, 1, number_of_points, f4,i*2-1);
 end
 %x = linspace(0,33,33);
+% This is a symtric null rull, even orders will result in zero offcourse
 figure;
-degrees = 1:30;
+degrees = (1:15)*2-1;
 subplot(2,1,1);
-plot(degrees,N1);hold all;
-plot(degrees,N2);
+semilogy(degrees,abs(N1));hold all;
+semilogy(degrees,abs(N2));
 legend('f1','f2');
 subplot(2,1,2);
-plot(degrees,N3);hold all;
-plot(degrees,N4);
+semilogy(degrees,abs(N3));hold all;
+semilogy(degrees,abs(N4));
 legend('f3','f4');
 hold off
