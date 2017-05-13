8916d4c838d9079a6032e2aa45c5d1f60d95a275
Willem Melis <zilleplus@gmail.com>
2017-05-13 19:41:12 +0200

    cleaned up the code, removed the for loop

--- oefz2/CompountTrapezoid.m
+++ oefz2/CompountTrapezoid.m
@@ -1,13 +1,18 @@
-function [ T ] = CompountTrapezoid( a,b,l,fun )
-%UNTITLED3 Summary of this function goes here
-%   Detailed explanation goes here
-h = (b-a)/l;
-T = (h/2)*(fun(a)+fun(b));
-temp = 0;
-for k=1:l-1
-   x(k) = a+(k)*h;
-   temp = temp + (fun(x(k)));
-end
-T = T + h*temp;
+function [ T ] = CompountTrapezoid( a,b,number_of_points,fun )
+interval_lenght=b-a;
+trap_rule = (interval_lenght/(number_of_points-1))*ones(1,number_of_points);
+trap_rule(1)=0.5*trap_rule(1); 
+trap_rule(end)=0.5*trap_rule(end); 
+x=linspace(-1,1,number_of_points);
+T=fun(x)*trap_rule';
+
+% h = (b-a)/l;
+% T = (h/2)*(fun(a)+fun(b));
+% temp = 0;
+% for k=1:l-1
+%    x(k) = a+(k)*h;
+%    temp = temp + (fun(x(k)));
+% end
+% T = T + h*temp;
 end
 
--- oefz2/nulregels_oef1.m
+++ oefz2/nulregels_oef1.m
@@ -1,16 +1,21 @@
 close all; clear;
+run('functies');
+number_of_points=31;
 
-f1 = @(x) x^20;  %exact 2/21
-f2 = @(x) exp(x); %exact exp(1)-exp(-1)
-f3 = @(x) exp(-x^2); % exact sqrt(PI)*erf(1)
-f4 = @(x) 1/(1+16*x^2); %exact arctan(4)/2
+sol1 = CompountTrapezoid(-1,1,number_of_points,f1);
+sol2 = CompountTrapezoid(-1,1,number_of_points,f2);
+sol3 = CompountTrapezoid(-1,1,number_of_points,f3);
+sol4 = CompountTrapezoid(-1,1,number_of_points,f4);
 
-sol1 = CompountTrapezoid(-1,1,31,f1);
-sol2 = CompountTrapezoid(-1,1,31,f2);
-sol3 = CompountTrapezoid(-1,1,31,f3);
-sol4 = CompountTrapezoid(-1,1,31,f4);
+error1 = abs(int_f1-sol1);
+error2 = abs(int_f2-sol2);
+error3 = abs(int_f3-sol3);
+error4 = abs(int_f4-sol4);
 
-error1 = abs((2/21)-sol1);
-error2 = abs((exp(1)-exp(-1))-sol2);
-error3 = abs(sqrt(pi)*erf(1)-sol3);
-error4 = abs(atan(4)/2-sol4);
+disp(['with ' num2str(number_of_points) ...
+    ' points there should at least be an accuracy of: ' ...
+    num2str(number_of_points^-1)])
+disp(['accuracy integrating f1: ' num2str(error1)]);
+disp(['accuracy integrating f2: ' num2str(error2)]);
+disp(['accuracy integrating f3: ' num2str(error3)]);
+disp(['accuracy integrating f4: ' num2str(error4)]);
