a3145f4325ac0c0e36328d1352ee22ceb1c0ca9e
Willem Melis <zilleplus@gmail.com>
2017-05-14 00:02:15 +0200

    added files to generate figures

---
+++ oefz2/generate_figures.m
@@ -0,0 +1,5 @@
+clc;clear all;close all;
+run('nulregels_oef4_6.m');
+run('nulregels_oef5_6.m');
+run('nulregels_oef3.m');
+close all;
---
+++ oefz2/img/README.txt
@@ -0,0 +1,3 @@
+folder containing generated figures
+
+Run generate_figures.m to generate the figures
