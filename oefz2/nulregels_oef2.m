ddda9250a953acaea3b7e2c5eb826222a62a1674
Willem Melis <zilleplus@gmail.com>
2017-05-13 21:10:49 +0200

    clead up the code a bit

--- oefz2/nulregels_oef2.m
+++ oefz2/nulregels_oef2.m
@@ -1,11 +1,13 @@
 close all; clear;
+run('functies');
+number_of_points=31;
 
-f1 = @(x) x^20;  %exact 2/21
-f2 = @(x) exp(x); %exact exp(1)-exp(-1)
-f3 = @(x) exp(-x^2); % exact sqrt(PI)*erf(1)
-f4 = @(x) 1/(1+16*x^2); %exact arctan(4)/2
+N1 = nullrule(-1, 1, number_of_points, f1);
+N2 = nullrule(-1, 1, number_of_points, f2);
+N3 = nullrule(-1, 1, number_of_points, f3);
+N4 = nullrule(-1, 1, number_of_points, f4);
 
-N1 = nullrule(-1, 1, 31, f1);
-N2 = nullrule(-1, 1, 31, f2);
-N3 = nullrule(-1, 1, 31, f3);
-N4 = nullrule(-1, 1, 31, f4);
+disp(num2str(N1));
+disp(num2str(N2));
+disp(num2str(N3));
+disp(num2str(N4));
--- oefz2/nulregels_oef4.m
+++ oefz2/nulregels_oef4.m
@@ -1,9 +1,5 @@
 close all; clear;
-
-f1 = @(x) x^20;  %exact 2/21
-f2 = @(x) exp(x); %exact exp(1)-exp(-1)
-f3 = @(x) exp(-x^2); % exact sqrt(PI)*erf(1)
-f4 = @(x) 1/(1+16*x^2); %exact arctan(4)/2
+run('functies');
 
 N1 = [];
 r1 = [];
